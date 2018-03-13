1de9da9ad4aafc302e3d0a21032287e9f969441a
Ravinder Bhardwaj <rkbhardwaj88@gmail.com>
2018-03-13 22:46:00 +0530

    updating package.json

--- client/webpack.prod.js
+++ client/webpack.prod.js
@@ -10,7 +10,7 @@ module.exports = merge(common, {
   plugins: [
     new UglifyJSPlugin(),
     new webpack.DefinePlugin({
-      'process.env.NODE_ENV': JSON.stringify(process.env.NODE_ENV)
+      'process.env.NODE_ENV': JSON.stringify('production')
     }),
     new OptimizeCssAssetsPlugin({
       assetNameRegExp: /\.css$/g,
