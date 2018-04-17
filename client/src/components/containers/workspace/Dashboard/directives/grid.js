cd5f9975687810a9425cbca3004df38d77aa209b
Ravinder Bhardwaj <rkbhardwaj88@gmail.com>
2018-04-17 10:32:17 +0530

    gridster files

--- client/src/components/containers/workspace/Dashboard/directives/grid.js
+++ client/src/components/containers/workspace/Dashboard/directives/grid.js
@@ -1,7 +1,5 @@
 import React, { Component } from 'react';
 import _ from 'lodash';
-
-import constants from '../../../../../common/constants';
 import { PanelUtils } from '../panels/panel_utils';
 import Panel from '../panels/panel';
 import GridsterView from '../views/gridsterView';
--- client/src/components/containers/workspace/Dashboard/views/gridsterView.js
+++ client/src/components/containers/workspace/Dashboard/views/gridsterView.js
@@ -1,5 +1,6 @@
 import React from 'react';
 import { Responsive, WidthProvider } from 'react-grid-layout';
+import constants from '../../../../../common/constants';
 
 const ResponsiveReactGridLayout = WidthProvider(Responsive);
 const { DASHBOARD_VIEW_MODE, GRID_DIMENSIONS } = constants.dashboard;
@@ -14,8 +15,8 @@ const GridsterView = (props) => {
     <ResponsiveReactGridLayout
       className="layout"
       layouts={layouts}
-      breakpoints={ ...GRID_DIMENSIONS.breakpoints }
-      cols={ ...GRID_DIMENSIONS.cols }
+      breakpoints={GRID_DIMENSIONS.breakpoints}
+      cols={GRID_DIMENSIONS.cols}
     >
       {items}
     </ResponsiveReactGridLayout>
